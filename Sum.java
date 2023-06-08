6ecb5114dd2b9b19f2e5c90ba1866f3047782dcd
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-06-08 23:24:15 +0900

    add

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 public class Sum {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 1; i <= 100; i++ ) {
+        for (int i = 1; i <= 100; i++) {
             sum += i;
         }
         System.out.println("合計は" + sum);
