c72090cd04e4cc3693638783fd1ddd8b316d561b
kaoyudna <121837465+kaoyudna@users.noreply.github.com>
2023-06-11 14:43:33 +0900

    add

---
+++ 2/Exercise1.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class Exercise1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(
+            new InputStreamReader(System.in)
+        );
+    }
+}
